041ff57515151c639a2a84994b95d121bdfdb0fd
Abhishek069 <abhishinde0723@gmail.com>
2025-09-07 16:33:33 +0530

    update code for font

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 const Header = () => {
   return (
-    <header className="bg-warning m-1 border border-white py-3 text-center">
+    <header className="bg-warning m-1 border border-white py-3 text-center d-flex justify-content-between">
       <h1 className="m-0 text-danger fw-bold" style={{ fontFamily: "cursive", fontSize: "2rem" }}>
         <span className="text-pink">Satta Matka </span>Aajj Tak
       </h1>
+      <button className="btn btn-primary">Login</button>
     </header>
   );
 };
