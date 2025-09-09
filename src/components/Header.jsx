5c2bd1378caa4175ab9b6f82441eefa89557c7ea
Nishant Singh <nishantsingh@Nishants-MacBook-Air.local>
2025-09-09 21:02:56 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+  const navigate =  useNavigate()
   return (
     <header className="bg-warning m-1 border border-white py-3 text-center d-flex justify-content-between">
       <h1 className="m-0 text-danger fw-bold" style={{ fontFamily: "cursive", fontSize: "2rem" }}>
         <span className="text-pink">Satta Matka </span>Aajj Tak
       </h1>
-      <button className="btn btn-primary">Login</button>
+      <button className="btn btn-primary" onClick={() => navigate("/login")} >Login</button>
     </header>
   );
 };
